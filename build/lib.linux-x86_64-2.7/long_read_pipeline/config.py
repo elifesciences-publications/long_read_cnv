aa2f48d573fa7ab073e0a5cb77be1af0e2a5904e
James Boocock <sfk2001@gmail.com>
2017-02-02 00:10:58 -0800

    hello

--- build/lib.linux-x86_64-2.7/long_read_pipeline/config.py
+++ build/lib.linux-x86_64-2.7/long_read_pipeline/config.py
@@ -1,4 +1,4 @@
-PILON_PATH="/home/james/src/java/pilon-1.21.jar"
+PILON_PATH="/Users/smilefreak/Programming/OpenSource/MyGitHub/long_read_pipeline/src/pilon-1.21.jar"
 SPEEDSEQ_EXEC="speedseq"
 BWA="bwa"
 SPLITREADBEDTOPE="/Users/smilefreak/validation_ira_hall/scripts/splitReadSamToBedpe"
